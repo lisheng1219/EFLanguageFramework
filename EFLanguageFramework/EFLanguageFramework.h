4b5e070292b6e19957c6eaf699310fd83a89c9c8
Lisheng <lisheng@eefung.com>
2018-06-28 14:36:11 +0800

    inti code

---
+++ EFLanguageFramework/EFLanguageFramework.h
@@ -0,0 +1,19 @@
+//
+//  EFLanguageFramework.h
+//  EFLanguageFramework
+//
+//  Created by lisheng on 28/06/2018.
+//  Copyright © 2018 Lisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EFLanguageFramework.
+FOUNDATION_EXPORT double EFLanguageFrameworkVersionNumber;
+
+//! Project version string for EFLanguageFrameworkFramework.
+FOUNDATION_EXPORT const unsigned char EFLanguageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EFLanguageFrameworkFramework/PublicHeader.h>
+
+
---
+++ EFLanguageFramework/NSBundle+EFLanauage.swift
@@ -0,0 +1,41 @@
+//
+//  NSBundle+EFLanauage.swift
+//  EFLanguageFramework
+//
+//  Created by lisheng on 12/03/2018.
+//  Copyright © 2018 Hunan EEfung Software Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  当调用onLanguage后替换掉mainBundle为当前语言的bundle
+ */
+class BundleEx: Bundle {
+    
+    override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+        if let bundle = languageBundle() {
+            return bundle.localizedString(forKey: key, value: value, table: tableName)
+        } else {
+            return super.localizedString(forKey: key, value: value, table: tableName)
+        }
+    }
+}
+
+extension Bundle {
+    
+    //代替dispatch_once
+    private static var onLanguageDispatchOnce: ()->Void = {
+        object_setClass(Bundle.main, BundleEx.self)
+    }
+    
+    func onLanguage() {
+        //替换NSBundle.mainBundle()的class为自定义的BundleEx，这样一来我们就可以重写方法
+        Bundle.onLanguageDispatchOnce()
+    }
+    
+    //当前语言的bundle
+    func languageBundle() -> Bundle? {
+        return EFLanguageFramework.standardLanguage().currentLanguageBundle
+    }
+}
